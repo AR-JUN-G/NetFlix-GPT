4a7c69022869fd4a01346d72c20e6f6211531f3d
AR-JUN-G <arjun07091102@gmail.com>
2024-07-04 00:51:20 +0530

    Added Validation using regex

---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+const validateSignUpDetails = (email, password) => {
+    const emailPattern = /^[a-zA-Z0-9_.±]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+    
+    const isEmailValid = emailPattern.test(email);
+    const isPasswordValid = passwordPattern.test(password);
+    
+    if (!isEmailValid) return "Email Id is Not Valid";
+    if (!isPasswordValid) return "Password is Not Valid";
+    
+    return null;
+}
+
+export {validateSignUpDetails};
