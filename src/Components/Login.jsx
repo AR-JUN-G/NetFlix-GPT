a7972ae59969cbff90939ce3ae0762502a56dc70
AR-JUN-G <arjun07091102@gmail.com>
2024-07-04 23:27:06 +0530

    Configured Firebase and Delployed the App

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -3,12 +3,13 @@ import Header from "./Header"
 import { validateSignUpDetails } from "../utils/validate";
 const Login=()=>{
     const [isSignInForm,setIsSignInForm]=useState(true);
+    const [errorMessage,setErrorMessage]=useState("");
     const email=useRef(null);
     const password=useRef(null);
 
     const handleSubmit=()=>{
         const message=validateSignUpDetails(email.current.value,password.current.value);
-        console.log(message);
+        setErrorMessage(message);
     }
     const handleSignUpClick=()=>{
         setIsSignInForm(!isSignInForm);
@@ -18,8 +19,9 @@ const Login=()=>{
         <div >
             <Header/>
             <img className="absolute" src="https://assets.nflxext.com/ffe/siteui/vlv3/335ddde7-3955-499c-b4cc-ca2eb7e1ae71/a7d20bc1-831c-4f9d-8153-11bdf7a08d23/IN-en-20240624-POP_SIGNUP_TWO_WEEKS-perspective_WEB_13cda806-d858-493e-b4aa-f2792ff965dc_large.jpg" alt="BannerLogo"/>
-            <form onsSubmit={(e)=>e.preventDefault()} className="w-3/12 p-14 absolute bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 ">
+            <form onSubmit={(e)=>e.preventDefault()} className="w-3/12 p-14 absolute bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 ">
                 <h1 className="font-bold text-3xl py-4 ">{isSignInForm?"Sign In":"Sign Up"}</h1>
+                <p className="text-red-500">{errorMessage.length?errorMessage:""}</p>
                 {!isSignInForm?<input type="text" placeholder="FullName" className="p-2 my-4 w-full rounded-sm bg-gray-700"/>:""}
                 <input  ref={email} type="text" placeholder="Email Address" className="p-2 my-4 w-full rounded-sm bg-gray-700 "/>
                 <input ref={password} type="password" placeholder="Password" className="p-2 my-4 w-full rounded-sm bg-gray-700"/>
---
+++ src/utils/fireBase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDIkwuxcxE8RiLWsreYNDkXdcPAvg9Db4M",
+  authDomain: "netflixgpt-1e782.firebaseapp.com",
+  projectId: "netflixgpt-1e782",
+  storageBucket: "netflixgpt-1e782.appspot.com",
+  messagingSenderId: "453567276265",
+  appId: "1:453567276265:web:72522c0cf18fc7030afc78",
+  measurementId: "G-DK74N4PZ58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
